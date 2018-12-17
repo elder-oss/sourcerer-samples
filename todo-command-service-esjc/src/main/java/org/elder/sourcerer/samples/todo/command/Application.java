a5590848281e631a4aa0a65eff932ee977d0bbf8
Kristian Freed <kristian@elder.org>
2018-12-17 09:43:29 +0000

    WIP

---
+++ todo-command-service-esjc/src/main/java/org/elder/sourcerer/samples/todo/command/Application.java
@@ -0,0 +1,59 @@
+package org.elder.sourcerer.samples.todo.command;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.Api;
+import org.elder.soucerer.eventstore.EventStoreConfiguration;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@Configuration
+@Import(EventStoreConfiguration.class)
+@ComponentScan("org.elder.sourcerer")
+public class Application {
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ObjectMapper getObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.findAndRegisterModules();
+        return mapper;
+    }
+
+    @Bean
+    public Docket customDocket() {
+        ApiInfo apiInfo = new ApiInfo(
+                "TODO Command Service",
+                "REST service implementation for creating and modifying TODO items",
+                "1.0.0",
+                null,
+                new Contact(
+                        "Elder Developers",
+                        "https://www.github.com/elder-oss",
+                        "tech@elder.org"),
+                null,
+                null);
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .build()
+                .apiInfo(apiInfo);
+    }
+}
